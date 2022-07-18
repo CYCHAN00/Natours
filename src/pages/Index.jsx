188a34b7279d46d5a8adff041e7036f48b9d585f
Chan Chun Yu <itroychan@gmail.com>
2022-07-18 21:57:17 +0800

    build navigation popup

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,12 +1,15 @@
 import React, { Fragment } from 'react';
-
+import Navigation from '../components/Navigation';
+import Footer from '../layout/Footer';
 import Header from '../layout/Header';
 import Main from '../layout/Main';
 const Index = () => {
   return (
     <Fragment>
+      <Navigation></Navigation>
       <Header></Header>
       <Main></Main>
+      <Footer></Footer>
     </Fragment>
   );
 };
